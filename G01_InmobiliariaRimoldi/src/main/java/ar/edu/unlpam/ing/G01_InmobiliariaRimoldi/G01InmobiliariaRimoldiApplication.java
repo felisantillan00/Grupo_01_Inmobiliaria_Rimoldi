8986bdd4482d95cdc24e63311f82ad512f8299a0
Felipe Santillan <130380077+felisantillan00@users.noreply.github.com>
2025-09-23 10:36:38 -0300

    Subiendo proyecto inicializado y diferentes documentacion.

---
+++ G01_InmobiliariaRimoldi/src/main/java/ar/edu/unlpam/ing/G01_InmobiliariaRimoldi/G01InmobiliariaRimoldiApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unlpam.ing.G01_InmobiliariaRimoldi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G01InmobiliariaRimoldiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G01InmobiliariaRimoldiApplication.class, args);
+	}
+
+}
